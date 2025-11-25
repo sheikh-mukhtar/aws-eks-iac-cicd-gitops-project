281b50e6088cbb2e2fb89d5969030d019c2272b2
sheikh-mukhtar <mukhtarsheikhh05@gmail.com>
2025-11-25 22:11:29 +0530

    project-infra

---
+++ terraform-jenkins-agent-setup/variables.tf
@@ -0,0 +1,19 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_name" {
+  type        = string
+  default     = "mukhtarsheikh.site"
+  description = "description"
+}
+
+variable "zone_id" {
+  type        = string
+  default     = "Z04204943MK4WM5RN7KUF"
+  description = "description"
+}
